34e766fd9f91ef7ab044f7b3bdbadc66bf3b170e
priyanka1706 <priyankachandrashekhar1706@gmail.com>
2018-08-28 12:04:17 +0530

    Added "Assets" files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static int currentScore;
+	public static int highscore;
+
+	public static int currentLevel = 0;
+	public static int unlockedLevel;
+
+	public static void CompleteLevel()
+	{
+		if (currentLevel < 3)
+		{
+			currentLevel += 1;
+			Application.LoadLevel(currentLevel);
+			print (currentLevel - 1);
+		}
+
+		else
+		{
+			print("You Win");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	public float moveSpeed;
+	public GameObject deathParticles;
+
+	private float maxSpeed = 6f;
+	private Vector3 input;
+	private Vector3 spawn;
+
+	// Use this for initialization
+	void Start () {
+		spawn = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+		{
+			GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+		}
+
+		if (transform.position.y < -2) {
+			Die ();
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other) //what we hit is stored in other
+	//enter hence triggered only at start
+	//if you want continuous, try oncollisionstay
+	{
+		if (other.transform.tag=="Ogre")
+		{
+			Die ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.tag == "Goal") {
+			GameManager.CompleteLevel ();
+		}
+	}
+
+	void Die()
+	{
+		Instantiate (deathParticles, transform.position, Quaternion.Euler (270, 0, 0));
+		//method to create an instance of the object in our world.
+		transform.position = spawn;
+	}
+}
